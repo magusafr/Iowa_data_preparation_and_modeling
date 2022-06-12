a203bb5485cde0fdf28c5eca6e301a538fc032e0
magusafr <40602197+magusafr@users.noreply.github.com>
2022-06-12 11:44:45 +0700

    Update product.sql

--- queries/data_modeling/product.sql
+++ queries/data_modeling/product.sql
@@ -18,38 +18,38 @@ FROM (
 	 ) AS product_dimension
 
 
---- Cleaning category
+-- Cleaning category
 --- "American Cordials & Liqueurs"
-UPDATE product SET category_name = "American Cordials & Liqueurs" WHERE category_name = "American Cordials & Liqueur";
-UPDATE product SET category_number = "1081000" WHERE category_number = "1081300"
+UPDATE product SET category_name = 'American Cordials & Liqueurs' WHERE category_name = 'American Cordials & Liqueur';
+UPDATE product SET category_number = '1081000' WHERE category_number = '1081300';
 
 --- "American Distilled Spirits Specialty"
-UPDATE product SET category_name = "American Distilled Spirits Specialty" WHERE category_name = "American Distilled Spirit Specialty";
-UPDATE product SET category_number = "1091000" WHERE category_number = "1091100"
+UPDATE product SET category_name = 'American Distilled Spirits Specialty' WHERE category_name = 'American Distilled Spirit Specialty';
+UPDATE product SET category_number = '1091000' WHERE category_number = '1091100';
 
 --- "American Vodkas"
-UPDATE product SET category_name = "American Vodkas" WHERE category_name = "American Vodka";
-UPDATE product SET category_number = "1031100" WHERE category_number = "1031000"
+UPDATE product SET category_name = 'American Vodkas' WHERE category_name = 'American Vodka';
+UPDATE product SET category_number = '1031100' WHERE category_number = '1031000';
 
 --- "Cocktails /Rtd"
-UPDATE product SET category_name = "Cocktails /Rtd" WHERE category_name = "Cocktails / Rtd";
-UPDATE product SET category_number = "1071100" WHERE category_number = "1070000"
+UPDATE product SET category_name = 'Cocktails /Rtd' WHERE category_name = 'Cocktails / Rtd';
+UPDATE product SET category_number = '1071100' WHERE category_number = '1070000';
 
 --- "Imported Cordials & Liqueurs"
-UPDATE product SET category_name = "Imported Cordials & Liqueurs" WHERE category_name = "Imported Cordials & Liqueur";
-UPDATE product SET category_number = "1082000" WHERE category_number = "1082100"
+UPDATE product SET category_name = 'Imported Cordials & Liqueurs' WHERE category_name = 'Imported Cordials & Liqueur';
+UPDATE product SET category_number = '1082000' WHERE category_number = '1082100';
 
 --- "Imported Distilled Spirits Specialty"
-UPDATE product SET category_name = "Imported Distilled Spirits Specialty" WHERE category_name = "Imported Distilled Spirit Specialty";
-UPDATE product SET category_number = "1092000" WHERE category_number = "1092100"
+UPDATE product SET category_name = 'Imported Distilled Spirits Specialty' WHERE category_name = 'Imported Distilled Spirit Specialty';
+UPDATE product SET category_number = '1092000' WHERE category_number = '1092100';
 
 --- "Imported Vodkas"
-UPDATE product SET category_name = "Imported Vodkas" WHERE category_name = "Imported Vodka";
-UPDATE product SET category_number = "1032100" WHERE category_number = "1032000"
+UPDATE product SET category_name = 'Imported Vodkas' WHERE category_name = 'Imported Vodka';
+UPDATE product SET category_number = '1032100' WHERE category_number = '1032000';
 
 --- "Temporary & Specialty Packages"
-UPDATE product SET category_name = "Temporary & Specialty Packages" WHERE category_name = "Temporary &  Specialty Packages";
-UPDATE product SET category_number = "1701100" WHERE category_number = "1700000"
+UPDATE product SET category_name = 'Temporary & Specialty Packages' WHERE category_name = 'Temporary &  Specialty Packages';
+UPDATE product SET category_number = '1701100' WHERE category_number = '1700000';
 
-UPDATE product SET category_name = "Temporary & Specialty Packages" WHERE category_name = "Temporary  & Specialty Packages";
-UPDATE product SET category_number = "1701100" WHERE category_number = "1070100"
+UPDATE product SET category_name = 'Temporary & Specialty Packages' WHERE category_name = 'Temporary  & Specialty Packages';
+UPDATE product SET category_number = '1701100' WHERE category_number = '1070100';
